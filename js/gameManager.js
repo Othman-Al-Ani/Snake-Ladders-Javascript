a9b21eba1a76443dd60adbb1fec0668bdcdd419b
WarMolee <142778225+WarMolee@users.noreply.github.com>
2023-09-14 11:20:30 +0200

    Co-authored-by: Simon Jonasson <4CEspX@users.noreply.github.com> Co-authored-by: Othman Al-Ani <Othman-Al-Ani@users.noreply.github.com> Co-authored-by: Ebbsteer <Ebbsteer@users.noreply.github.com>

--- js/gameManager.js
+++ js/gameManager.js
@@ -1,3 +1,13 @@
+function delay(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+}
+//Default player position
+var playerPos = 1;
+
+var currentlyMoving = false;
+
 var diceImg = document.getElementById("diceImg");
 var gameBoard = document.getElementById("gameboard");
 //var diceNumb = rollDice();
@@ -6,28 +16,52 @@ console.log("ur mom");
 
 diceImg.addEventListener('click', rollDice);
 
-
 function rollDice() {
-    var animation = 1;
+    if(currentlyMoving) return;
     var rand = Math.floor(Math.random() * 6 + 1);
-    /*for (i = 10; i > 0; i--) {
-        animation = Math.floor(Math.random() * 6 + 1);
-        diceImg.src = "Dices/Dice" + animation + ".svg"
-        console.log(animation);
-    } */
+
 
     diceImg.src = "../Dices/Dice" + rand + ".svg";
-
+    movePlayerDelay(rand);
+    currentlyMoving = true;
     return rand;
-}
 
+}
+function movePlayer(value) {
+    
+}
 
 function loadGame() {
-    for(i = 100; i > 0; i--)
-    {
-        var temp = document.createElement ("div");
+    for (i = 100; i > 0; i--) {
+
+        var temp = document.createElement("div");
         temp.id = i;
+        if (i == 1) {
+            var player = document.createElement("div");
+            player.id = "player";
+            temp.appendChild(player);
+        }
         gameBoard.appendChild(temp);
-        console.log(i);
+
     }
 }
+
+async function movePlayerDelay(value) {
+    for (i = 1; i <= value; i++) {
+        var div = document.getElementById(playerPos);
+        var nextDiv = document.getElementById(playerPos + 1);
+        //Delete player div from cell
+        console.log(div);
+        await delay(200); // This will pause execution for 2000 milliseconds (2 seconds)
+        div.innerHTML = " ";
+
+        var newPlayerDiv = document.createElement("div");
+        newPlayerDiv.id = "player";
+        nextDiv.appendChild(newPlayerDiv);
+        playerPos++;
+
+    }
+    currentlyMoving = false;
+
+}
+
--- js/ui.js
+++ js/ui.js
@@ -11,3 +11,37 @@ document.getElementById("volume").addEventListener("click", mute);
                 element.classList.toggle("bx-volume-full");
             }
         }
+
+
+
+        document.getElementById("question").addEventListener("click", tut);
+
+        function tut(){
+
+            var el = document.getElementById("tutorial");
+
+            if(el.classList.contains("hide")) {
+
+                el.classList.toggle("hide");
+            } else {
+                el.classList.toggle("hide");
+            
+    
+            }
+        }
+
+        document.getElementById("play").addEventListener("click", playset);
+
+        function playset(){
+
+            var ps = document.getElementById("play-settings");
+
+            if(ps.classList.contains("hide")) {
+
+                ps.classList.toggle("hide");
+            } else {
+                ps.classList.toggle("hide");
+            
+    
+            }
+        }
